a7d6144b5faab9a81208cf99bfd536f4ed3774b7
Coin-boy <ekomabasiuk@gmail.com>
2023-03-12 19:16:53 -0700

    tic-tac-toe
    little changes

--- script.js
+++ script.js
@@ -80,5 +80,7 @@ for(let i = 0; i <= 8; i++){
     };
 
     button.addEventListener('click',function(){
-        reset()
+        button.style.backgroundColor = 'lightblue';
+        reset();
+        
     })
